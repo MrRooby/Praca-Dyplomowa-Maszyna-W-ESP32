0b9f17f42a2d4a8df171e988e4c41f870787ab15
MrRooby <mrruby64@gmail.com>
2026-01-12 23:38:17 +0100

    duzo poprawek typu pierdoly

--- dowejpdf.sh
+++ dowejpdf.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+start=$(date +%s.%N)
+
 make
 make clean
 clear
 texcount -merge -sum main.tex | grep "Words in text"
+
+end=$(date +%s.%N)
+runtime=$(echo "scale=3; ($end - $start) / 1" | bc)
+echo -e "_________________________\n"
+echo "Compilation time: $runtime s"
